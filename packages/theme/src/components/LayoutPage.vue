4b127aaf108124345d3496a4bf8c463c9563e806
fengwang <1272171893@qq.com>
2023-04-05 23:40:46 +0800

    feat(theme): ✨ 修改theme的包自定义内容

--- packages/theme/src/components/LayoutPage.vue
+++ packages/theme/src/components/LayoutPage.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <h1>Custom Layout11!</h1>
-  </div>
+  <h1>Custom Layout!</h1>
+  <Layout>
+    <template #aside-outline-before> My custom sidebar top content </template>
+  </Layout>
+  <div v-if="frontmatter.layout === 'home'">Custom home page!</div>
+  <div v-if="page.isNotFound">Custom 404 page!</div>
+  <Content v-else />
 </template>
+<script lang="ts" setup>
+import Theme from 'vitepress/theme';
+import { useData } from 'vitepress';
+const { Layout } = Theme;
+const { page, frontmatter } = useData();
+</script>
