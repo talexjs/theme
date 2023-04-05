e8e9fe690fdceb49c7fa4634f63a837bbc9648bf
fengwang <1272171893@qq.com>
2023-04-05 23:23:53 +0800

    docs: 📃 修改使用文档

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,16 +1,2 @@
-// https://vitepress.dev/guide/custom-theme
-import { h } from 'vue';
-import Theme from 'vitepress/theme';
-import './style.css';
-
-export default {
-  ...Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    });
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  },
-};
+import themes from '@talexjs/theme';
+export default themes;
