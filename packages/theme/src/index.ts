33a870c91a17c08229543422ba82ff75df542113
fengwang <1272171893@qq.com>
2023-04-16 11:17:14 +0800

    feat: ✨ 添加 ghp 注释

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -3,3 +3,5 @@ import type { UserConfig, DefaultTheme } from 'vitepress';
 export const defineConfig = (options: UserConfig<DefaultTheme.Config> = {}) => {
   return Config(options);
 };
+
+// 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
